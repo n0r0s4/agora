143a4ecb8194f3a75da72aedb6eb6c4ab66b455d
norosa norosez <norosa@programmer.net>
2017-05-17 23:24:28 +0200

    added constraints to database to keep integrity

--- daw1704_last_constraints.sql
+++ daw1704_last_constraints.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 4.6.5.2
--- https://www.phpmyadmin.net/
+-- version 4.5.4.1
+-- http://www.phpmyadmin.net
 --
--- Servidor: 127.0.0.1
--- Tiempo de generación: 07-05-2017 a las 19:59:26
--- Versión del servidor: 10.1.21-MariaDB
--- Versión de PHP: 5.6.30
+-- Servidor: localhost
+-- Tiempo de generación: 17-05-2017 a las 23:22:50
+-- Versión del servidor: 5.6.33-0ubuntu0.14.04.1
+-- Versión de PHP: 7.0.18-1+deb.sury.org~trusty+1
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
@@ -39,9 +39,17 @@ CREATE TABLE `answers` (
 --
 
 INSERT INTO `answers` (`idanswer`, `nickname`, `idquestion`, `input`, `date`) VALUES
-(1, 'Lola', 1, 'Palindrome are those String whose reverse is equal to original.This can be done by using either Stri', '2017-05-17'),
 (2, 'Pepe', 2, 'you can remove a given character from String by converting it into character array and then using su', '2017-05-24'),
-(3, 'Xmen', 3, 'String is final because of same reason it is immutable. Couple of reasons which I think make sense i', '2017-05-23');
+(3, 'Xmen', 3, 'String is final because of same reason it is immutable. Couple of reasons which I think make sense i', '2017-05-23'),
+(4, 'anonymous', 1, 'wdf is a palindrome niggy', '2017-05-15'),
+(5, 'tester', 19, 'Yes bro, this definetively works!', '2017-05-15'),
+(6, 'anonymous', 1, 'i have an author, amazing!', '2017-05-15'),
+(7, 'anonymous', 2, 'Xq queda question 2  y are you a ...nCortado?', '2017-05-16'),
+(8, 'anonymous', 32, 'Broh that\'s because you are latino. I will pray omega the strong for your hot soul', '2017-05-16'),
+(10, 'anonymous', 4, 'test answer', '2017-05-16'),
+(11, 'anonymous', 33, 'with taw tool', '2017-05-17'),
+(12, 'anonymous', 34, 'Poleeeee and I take sit. waiting for questions. Kinda interesting brahs.', '2017-05-17'),
+(13, 'OmegaElFuerte', 32, 'Poleeee. Waiting answers!', '2017-05-17');
 
 -- --------------------------------------------------------
 
@@ -69,7 +77,10 @@ INSERT INTO `questions` (`idquestion`, `nickname`, `topicname`, `input`, `date`)
 (5, 'TheRock', 'Economy', 'Why Char array is preferred over String for storing password?', '2017-02-13'),
 (19, 'tester', 'C#', 'it works', '2017-05-06'),
 (29, 'anonymous', 'Java', 'how can i learn java easy mode easy pace? thx', '2017-05-06'),
-(30, 'tester', 'Java', 'Where can i find good springboot tutorialsÂ¿', '2017-05-07');
+(30, 'tester', 'Java', 'Where can i find good springboot tutorialsÂ¿', '2017-05-07'),
+(32, 'Aeros', 'Economy', 'Why i break consolas? i see the girls dancing alone and i cant avoid to break all the consoles i see', '2017-05-16'),
+(33, 'anonymous', 'Flu', 'how can i validate taw?', '2017-05-17'),
+(34, 'anonymous', 'C#', 'Why pink fiction is a good film? Is that because appears pink peaople in the film? #seriousDeal', '2017-05-17');
 
 -- --------------------------------------------------------
 
@@ -91,7 +102,6 @@ CREATE TABLE `reporta` (
 
 INSERT INTO `reporta` (`idreporta`, `nickname`, `idanswer`, `reporttext`, `date`) VALUES
 (1, 'Lola', 2, 'incorrect topic', '2017-06-15'),
-(2, 'Pepe', 2, 'incorrect topic', '2017-06-15'),
 (3, 'Xmen', 2, 'incorrect topic', '2017-07-18'),
 (4, 'Snoopy', 2, 'incorrect topic', '2017-06-21'),
 (5, 'TheRock', 2, 'incorrect topic', '2017-07-19');
@@ -115,7 +125,6 @@ CREATE TABLE `reportq` (
 --
 
 INSERT INTO `reportq` (`idreportq`, `nickname`, `idquestion`, `reporttext`, `date`) VALUES
-(1, 'Lola', 1, 'incorrect topic', '2017-06-16'),
 (2, 'Pepe', 1, 'incorrect topic', '2017-06-30'),
 (3, 'Xmen', 1, 'incorrect topic', '2017-06-23'),
 (4, 'Snoopy', 1, 'incorrect topic', '2017-06-17'),
@@ -164,12 +173,16 @@ CREATE TABLE `users` (
 --
 
 INSERT INTO `users` (`nickname`, `userscore`, `firstname`, `lastname`, `email`, `password`, `postalcode`) VALUES
+('Aeros', 0, 'Aeros', 'Serio ElReal', 'aeros@programmer.net', '5baa61e4c9b93f3f0682250b6cf8331b7ee68fd8', 8940),
 ('anonymous', 0, 'anonymous', 'anonymous', 'not@real.com', '40bd001563085fc35165329ea1ff5c5ecbdbbeef', 0),
 ('Lola', 2, 'Lola', 'Loli', 'loli@gmail.com', '40bd001563085fc35165329ea1ff5c5ecbdbbeef', 45623),
+('OmegaElFuerte', 0, 'Omar', 'ElFuerte', 'omarElFuerte@sempre.', '40bd001563085fc35165329ea1ff5c5ecbdbbeef', 123),
 ('Pepe', 3, 'Pepe', 'Väth', 'pepe@gmail.com', '40bd001563085fc35165329ea1ff5c5ecbdbbeef', 45623),
 ('Snoopy', 50, 'Snoopy', 'Loli', 'snoopy@gmail.com', '40bd001563085fc35165329ea1ff5c5ecbdbbeef', 45623),
 ('succesfullreal', 0, 'succesrealName', 'succesrealLastname', 'succesreal@mail.com', '40bd001563085fc35165329ea1ff5c5ecbdbbeef', 8004),
 ('tester', 0, 'tester', 'tester', 'n@mail.com', '40bd001563085fc35165329ea1ff5c5ecbdbbeef', 12345),
+('testera', 0, 'test', 'test', 'joh@mail.com', '40bd001563085fc35165329ea1ff5c5ecbdbbeef', 5080),
+('testeri', 0, 'test', 'tester', 'test@mail.com', '40bd001563085fc35165329ea1ff5c5ecbdbbeef', 12345),
 ('TheRock', 20, 'TheRock', 'Pierdra', 'marcos@gmail.com', '40bd001563085fc35165329ea1ff5c5ecbdbbeef', 78945),
 ('Xmen', 5, 'Xmen', 'Pescador', 'rocki@gmail.com', '40bd001563085fc35165329ea1ff5c5ecbdbbeef', 45632);
 
@@ -186,17 +199,6 @@ CREATE TABLE `valorationa` (
   `valoration` int(4) DEFAULT NULL,
   `date` date DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci;
-
---
--- Volcado de datos para la tabla `valorationa`
---
-
-INSERT INTO `valorationa` (`idvalorationa`, `nickname`, `idanswer`, `valoration`, `date`) VALUES
-(1, 'Lola', 1, 5, '2017-04-26'),
-(2, 'Pepe', 1, 4, '2017-06-22'),
-(3, 'Xmen', 1, 3, '2017-06-14'),
-(4, 'Snoopy', 1, 4, '2017-06-18'),
-(5, 'TheRock', 1, 5, '2017-06-20');
 
 -- --------------------------------------------------------
 
@@ -302,12 +304,12 @@ ALTER TABLE `valorationq`
 -- AUTO_INCREMENT de la tabla `answers`
 --
 ALTER TABLE `answers`
-  MODIFY `idanswer` int(4) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `idanswer` int(4) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
 --
 -- AUTO_INCREMENT de la tabla `questions`
 --
 ALTER TABLE `questions`
-  MODIFY `idquestion` int(4) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=31;
+  MODIFY `idquestion` int(4) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=35;
 --
 -- AUTO_INCREMENT de la tabla `reporta`
 --
@@ -322,7 +324,7 @@ ALTER TABLE `reportq`
 -- AUTO_INCREMENT de la tabla `valorationa`
 --
 ALTER TABLE `valorationa`
-  MODIFY `idvalorationa` int(4) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+  MODIFY `idvalorationa` int(4) NOT NULL AUTO_INCREMENT;
 --
 -- AUTO_INCREMENT de la tabla `valorationq`
 --
@@ -337,7 +339,7 @@ ALTER TABLE `valorationq`
 --
 ALTER TABLE `answers`
   ADD CONSTRAINT `answers_ibfk_1` FOREIGN KEY (`nickname`) REFERENCES `users` (`nickname`),
-  ADD CONSTRAINT `answers_ibfk_2` FOREIGN KEY (`idquestion`) REFERENCES `questions` (`idquestion`);
+  ADD CONSTRAINT `answers_ibfk_2` FOREIGN KEY (`idquestion`) REFERENCES `questions` (`idquestion`) ON DELETE CASCADE ON UPDATE CASCADE;
 
 --
 -- Filtros para la tabla `questions`
@@ -350,7 +352,7 @@ ALTER TABLE `questions`
 -- Filtros para la tabla `reporta`
 --
 ALTER TABLE `reporta`
-  ADD CONSTRAINT `reporta_ibfk_1` FOREIGN KEY (`idanswer`) REFERENCES `answers` (`idanswer`),
+  ADD CONSTRAINT `reporta_ibfk_1` FOREIGN KEY (`idanswer`) REFERENCES `answers` (`idanswer`) ON DELETE CASCADE ON UPDATE CASCADE,
   ADD CONSTRAINT `reporta_ibfk_2` FOREIGN KEY (`nickname`) REFERENCES `users` (`nickname`);
 
 --
@@ -363,14 +365,14 @@ ALTER TABLE `reportq`
 -- Filtros para la tabla `valorationa`
 --
 ALTER TABLE `valorationa`
-  ADD CONSTRAINT `valorationa_ibfk_1` FOREIGN KEY (`idanswer`) REFERENCES `answers` (`idanswer`),
+  ADD CONSTRAINT `valorationa_ibfk_1` FOREIGN KEY (`idanswer`) REFERENCES `answers` (`idanswer`) ON DELETE CASCADE ON UPDATE CASCADE,
   ADD CONSTRAINT `valorationa_ibfk_2` FOREIGN KEY (`nickname`) REFERENCES `users` (`nickname`);
 
 --
 -- Filtros para la tabla `valorationq`
 --
 ALTER TABLE `valorationq`
-  ADD CONSTRAINT `valorationq_ibfk_1` FOREIGN KEY (`idquestion`) REFERENCES `questions` (`idquestion`),
+  ADD CONSTRAINT `valorationq_ibfk_1` FOREIGN KEY (`idquestion`) REFERENCES `questions` (`idquestion`) ON DELETE CASCADE ON UPDATE CASCADE,
   ADD CONSTRAINT `valorationq_ibfk_2` FOREIGN KEY (`nickname`) REFERENCES `users` (`nickname`);
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
